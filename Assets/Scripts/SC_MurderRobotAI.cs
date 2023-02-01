59751336fa7d1cef8705d53da267913a7ac59b1d
Benoit <98055777+Noxtibe@users.noreply.github.com>
2023-02-01 12:08:44 +0100

    [Added] Change Color but no conversion sniff

--- Assets/Scripts/SC_MurderRobotAI.cs
+++ Assets/Scripts/SC_MurderRobotAI.cs
@@ -1,49 +1,124 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class SC_MurderRobotAI : MonoBehaviour
 {
     private NavMeshAgent _agent;
-    //[SerializeField] private Transform _targets; // For a single point
-    [SerializeField] private List<Transform> _targets; // For multiple points
+    [SerializeField] private List<GameObject> _targets; // For multiple points
 
     private int current = 0;
+
+    private GameObject enemy;
     public int delay = 3;
     public float distanceTreshold = 1.0f;
+
+    public bool redTeam = false;
+    public bool blueTeam = false;
 
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
-        _agent.SetDestination(_targets[current].position);
+        _targets = GameObject.FindGameObjectsWithTag("Path").ToList();
+
+        // Shuffling the path
+        System.Random rand = new System.Random();
+        _targets = _targets.OrderBy(e => rand.Next()).ToList();
+
+        MoveToNextTarget();
+
+        if(gameObject.tag == "Blue")
+        {
+            blueTeam = true;
+        }
+        ChangeTeamColor(blueTeam);
     }
 
     void Update()
     {
-        if(Vector3.Distance(transform.position, _targets[current].position) <= distanceTreshold)
+        if(Vector3.Distance(transform.position, _targets[current].transform.position) <= distanceTreshold)
         {
-             current = (current +1)%(_targets.Count);
+            current = (current +1)%(_targets.Count);
             Invoke("MoveToNextTarget", delay);
-            //_agent.isStopped = true; // Stop the AI
+        }
+
+        if(enemy != null)
+        {
+            _agent.SetDestination(enemy.transform.position);
+            if (Vector3.Distance(transform.position, enemy.transform.position) <= distanceTreshold)
+            {
+                enemy.GetComponent<SC_MurderRobotAI>().Convert();
+                enemy = null;
+                MoveToNextTarget();
+            }
         }
     }
 
     void MoveToNextTarget()
     {
-        _agent.SetDestination(_targets[current].position);
+        _agent.SetDestination(_targets[current].transform.position);
+    }
+
+    void Convert()
+    {
+        enemy = null;
+
+        if(blueTeam)
+        {
+            blueTeam = false;
+            gameObject.tag = "Red";
+        }
+        else
+        {
+            redTeam = false;
+            gameObject.tag = "Blue";
+        }
+        ChangeTeamColor(blueTeam);
+        
+        Invoke("MoveToNextTarget", delay);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Enemy"))
+        if(blueTeam)
         {
-            _agent.isStopped = true; // Stop the AI
+            if(gameObject.CompareTag("Red"))
+            {
+                enemy = other.gameObject;
+            }
+        }
+        else
+        {
+            if(gameObject.CompareTag("Blue"))
+            {
+                enemy = other.gameObject;
+            }
         }
     }
 
-    private void OnTriggerExit(Collider other)
+    // My method to change the colors
+    void ChangeTeamColor(bool blueTeam)
     {
-        _agent.isStopped = false;
+        if(blueTeam)
+        {
+            GameObject body = transform.GetChild(0).gameObject;
+            for(int i = 0; i < body.transform.childCount; i++)
+            {
+                body.transform.GetChild(i).GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+            }
+            body.transform.GetChild(1).GetChild(0).GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+        } 
+        else
+        {
+            GameObject body = transform.GetChild(0).gameObject;
+            for (int i = 0; i < body.transform.childCount; i++)
+            {
+                body.transform.GetChild(i).GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+            }
+            body.transform.GetChild(1).GetChild(0).GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        }
     }
 }
